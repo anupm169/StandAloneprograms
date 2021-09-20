dc9f09f6c11860d3e467090b6bd41d2ebc364d02
anupm1694 <anupm1694@gmail.com>
2021-09-20 12:29:47 +0530

    anup-stack : added Stack implementation

---
+++ src/com/zeenet/dataStructures/BasicStack.java
@@ -0,0 +1,54 @@
+package com.zeenet.dataStructures;
+
+public class BasicStack<X> {
+
+	private X[] data;
+
+	private int stackPointer;
+
+	@SuppressWarnings("unchecked")
+	public BasicStack() {
+		data = (X[]) new Object[1000];
+		stackPointer = 0;
+	}
+
+	public void push(X item) {
+		data[stackPointer++] = item;
+	}
+
+	public X pop() {
+		if (stackPointer == 0) {
+			throw new IllegalStateException("The Stack is Empty");
+		}
+
+		return data[--stackPointer];
+	}
+
+	public int size() {
+		return stackPointer;
+	}
+
+	public boolean contains(X item) {
+
+		for (int i = 0; i < size(); i++) {
+			if (data[i].equals(item)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public X access(X item) {
+
+		X stackItem = null;
+		while (stackPointer > -1) {
+			stackItem = pop();
+			if (stackItem.equals(item)) {
+				return stackItem;
+			}
+		}
+		throw new IllegalStateException("Specified elements could not be found");
+
+	}
+
+}
