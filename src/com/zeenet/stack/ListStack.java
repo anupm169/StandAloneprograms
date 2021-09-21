abbc53231d1e882fa964dd1ae6b128113e7c5552
anupm1694 <anupm1694@gmail.com>
2021-09-21 18:29:46 +0530

    anup-stack : added list interface to support stack

---
+++ src/com/zeenet/stack/ListStack.java
@@ -0,0 +1,58 @@
+package com.zeenet.stack;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListStack<X> {
+
+	List<X> list;
+
+	public ListStack() {
+		list = new ArrayList<X>();
+	}
+
+	public void push(X item) {
+		list.add(item);
+	}
+
+	public X pop() {
+
+		if (list.size() == 0) {
+			throw new IllegalStateException("the stack is empty");
+		}
+
+		X item = list.get(list.size() - 1);
+		list.remove(list.size() - 1);
+		return item;
+	}
+
+	public boolean containsitem(X item) {
+		boolean found = false;
+
+		for (int i = 0; i < list.size(); i++) {
+			if (item.equals(list.get(i))) {
+				found = true;
+			}
+		}
+		return found;
+
+	}
+
+	public X access(X item) {
+		X result = null;
+
+		while (list.size() > -1) {
+			result = pop();
+			if (result.equals(item)) {
+				return result;
+			}
+		}
+
+		throw new IllegalStateException("the request element was not found");
+	}
+
+	public String toString() {
+		return list.toString();
+	}
+
+}
--- src/com/zeenet/stack/StackMainClass.java
+++ src/com/zeenet/stack/StackMainClass.java
@@ -20,6 +20,29 @@ public class StackMainClass {
 		System.out.println("Is item 2 present : " + stack.contains(2));
 
 		System.out.println("trying to access the item : " + stack.access(1));
+		
+		System.out.println("============Stack using List Interface =============");
+
+		ListStack<Integer> listStack = new ListStack<>();
+
+		listStack.push(1);
+		listStack.push(2);
+		listStack.push(3);
+		listStack.push(4);
+		listStack.push(5);
+
+		System.out.println(listStack);
+
+		System.out.println("the deleted item in stack is : " + listStack.pop());
+		System.out.println("the deleted item in stack is : " + listStack.pop());
+		System.out.println("the deleted item in stack is : " + listStack.pop());
+		System.out.println("the deleted item in stack is : " + listStack.pop());
+
+		System.out.println("Is item 2 present : " + listStack.containsitem(2));
+
+		System.out.println("trying to access the item : " + listStack.access(1));
+		
+		System.out.println(listStack);
 
 	}
 
