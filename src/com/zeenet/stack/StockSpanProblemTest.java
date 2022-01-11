1bea7aed1fa5ce68ed79acba2d679da75d644140
anupm1694 <anupm1694@gmail.com>
2022-01-11 13:39:23 +0530

    anup-stack : added stockplan problem

---
+++ src/com/zeenet/stack/StockSpanProblemTest.java
@@ -0,0 +1,34 @@
+package com.zeenet.stack;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StockSpanProblemTest {
+
+	@Test
+	public void inputTestCases() {
+
+//		int[] price = { 70, 80, 100, 70, 90, 75, 85 };
+//
+//		int[] result = { 1, 2, 3, 1, 2, 1, 2 };
+//
+//		int[] expectedResult = StockSpanProblem.solveProblemUsingStack(price);
+//
+//		Assert.assertArrayEquals(result, expectedResult);
+
+	}
+
+	@Test
+	public void inputTestCasesWithDifferentInput() {
+
+//		int[] price = { 100, 80, 60, 70, 60, 75, 85 };
+//
+//		int[] result = { 1, 1, 1, 2, 1, 4, 6};
+//
+//		int[] expectedResult = StockSpanProblem.solveProblemUsingStack(price);
+//
+//		Assert.assertArrayEquals(result, expectedResult);
+
+	}
+
+}
